85189c235c09b57b7bc7d890ea85405524aa9b27
ivzhang1 <izhang1@stuy.edu>
2018-09-27 11:00:24 -0400

    added header file

---
+++ str.h
@@ -0,0 +1,11 @@
+int strlen(char * source);
+
+char* strncpy(char *dest, char *source, int n);
+
+char* strcat(char *dest, char *source);
+
+char* strcmp(char *s1, char *s2);
+
+char* strchr(char *s, char c);
+
+char* strstr(char *s1, char *s2);
